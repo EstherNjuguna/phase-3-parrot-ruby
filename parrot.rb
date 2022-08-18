9efff3c15628dc19e5a7d66104862073976722bd
+ <+>
2022-08-18 16:10:47 +0300

    commit tree 9b8df2ada91f374226ef996905dd9ec91eb462fa

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+    def parrot sounds ="Squawk!"
+        puts sounds
+    # returns sthe phrase
+        sounds
+    end
